6401e580fc78c47436e1e0216128c7feaafbd3f5
wooyoung-tom <tom@sendy.ai>
2021-03-10 22:50:32 +0900

    first commit

---
+++ src/main/java/wooyoung/tom/simplespringboot/SimpleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package wooyoung.tom.simplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringBootApplication.class, args);
+    }
+
+}
