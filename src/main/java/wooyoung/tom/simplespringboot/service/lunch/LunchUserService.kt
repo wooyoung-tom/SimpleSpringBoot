6a9072a5c49e4ac003930332132a57576c45d83f
wooyoung-tom <tom@sendy.ai>
2021-03-20 18:37:09 +0900

    git test

--- src/main/java/wooyoung/tom/simplespringboot/service/lunch/LunchUserService.kt
+++ src/main/java/wooyoung/tom/simplespringboot/service/lunch/LunchUserService.kt
@@ -46,7 +46,6 @@ open class LunchUserService(
             LunchUserResponse(
                 message = "${user.name} 등록을 완료하였습니다.",
                 body = saveResult
-
             )
         }
     }
